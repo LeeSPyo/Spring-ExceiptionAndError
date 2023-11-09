9c084282d3add5d0b39c5473460c4671f787be1e
LeeSPyo <oco141@naver.com>
2023-11-10 08:40:05 +0900

    API 예외 처리 - HandlerExceptionResolver 시작

--- exception/src/main/java/hello/exception/WebConfig.java
+++ exception/src/main/java/hello/exception/WebConfig.java
@@ -1,18 +1,29 @@
 package hello.exception;
 
+import java.util.List;
+
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.HandlerExceptionResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import hello.exception.filter.LogFilter;
 import hello.exception.interceptor.LogInterceptor;
+import hello.exception.resolver.MyHandlerExceptionResolver;
 import jakarta.servlet.DispatcherType;
 import jakarta.servlet.Filter;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+
+	/**
+	 * 기본 설정을 유지하면서 추가
+	 */
+	@Override
+	public void extendHandlerExceptionResolvers(List<HandlerExceptionResolver> resolvers) {
+		resolvers.add(new MyHandlerExceptionResolver());
+	}
 
 	@Override
 	public void addInterceptors(InterceptorRegistry registry) {
@@ -21,7 +32,7 @@ public class WebConfig implements WebMvcConfigurer {
 		);
 	}
 
-	//@Bean
+	// @Bean
 	public FilterRegistrationBean logFilter() {
 		FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean<>();
 		filterRegistrationBean.setFilter(new LogFilter());
